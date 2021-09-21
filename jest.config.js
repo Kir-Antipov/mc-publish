98b2172a778b905121123934aa6fb3e6f083fe8b
Kir_Antipov <kp.antipov@gmail.com>
2021-09-21 10:27:32 +0300

    Setup Jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    clearMocks: true,
+    moduleFileExtensions: ["js", "ts"],
+    testMatch: ["**/*.test.ts"],
+    transform: { ".(t|j)s$": "babel-jest" },
+    verbose: true
+};
