c9638e15f2831a58bb6ccaf74ff594190b7ed4dc
Kir_Antipov <kp.antipov@gmail.com>
2021-09-25 15:45:44 +0300

    Covered curseforge-utils with tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
     moduleFileExtensions: ["js", "ts"],
     testMatch: ["**/*.test.ts"],
     transform: { ".(t|j)s$": "babel-jest" },
+    transformIgnorePatterns: ["/node_modules/(?!node-fetch|fetch-blob).+\\.js$"],
+    moduleNameMapper: {
+        "formdata-node/file-from-path": "<rootDir>node_modules/formdata-node/lib/cjs/fileFromPath"
+    },
     verbose: true
 };
