9effd57e821e5d0278fce624ea6bc6bb6766740f
Kir_Antipov <kp.antipov@gmail.com>
2021-12-07 21:58:09 +0300

    Covered ModMetadataReaderFactory with tests

---
+++ test/mod-metadata-reader-factory.test.ts
@@ -0,0 +1,18 @@
+import { describe, test, expect } from "@jest/globals";
+import ModLoaderType from "../src/metadata/mod-loader-type";
+import ModMetadataReaderFactory from "../src/metadata/mod-metadata-reader-factory";
+
+describe("ModMetadataReaderFactory.create", () => {
+    test("factory can create metadata reader for every ModLoaderType value", () => {
+        const factory = new ModMetadataReaderFactory();
+        for (const target of ModLoaderType.getValues()) {
+            const reader = factory.create(target);
+            expect(reader).toHaveProperty("readMetadata");
+        }
+    });
+
+    test("the method throws on invalid ModLoaderType value", () => {
+        const factory = new ModMetadataReaderFactory();
+        expect(() => factory.create(-1)).toThrow();
+    });
+});
