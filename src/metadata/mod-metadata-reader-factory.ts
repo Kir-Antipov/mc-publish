05a29057caccf2b29a26cc4899f2eca5841486b5
Kir_Antipov <kp.antipov@gmail.com>
2021-12-07 21:53:08 +0300

    Implemented ModMetadataReader for Fabric mods

--- src/metadata/mod-metadata-reader-factory.ts
+++ src/metadata/mod-metadata-reader-factory.ts
@@ -1,9 +1,13 @@
+import FabricModMetadataReader from "./fabric/fabric-mod-metadata-reader";
 import ModLoaderType from "./mod-loader-type";
 import ModMetadataReader from "./mod-metadata-reader";
 
 export default class ModMetadataReaderFactory {
     public create(loaderType: ModLoaderType): ModMetadataReader {
         switch (loaderType) {
+            case ModLoaderType.Fabric:
+                return new FabricModMetadataReader();
+
             default:
                 throw new Error(`Unknown mod loader "${ModLoaderType.toString(loaderType)}"`);
         }
