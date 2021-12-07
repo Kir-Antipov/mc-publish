59f286e65e9c00fd2adf1d2c6f09ac8c7f720ec3
Kir_Antipov <kp.antipov@gmail.com>
2021-12-07 21:32:27 +0300

    Made enum that describes different mod loaders

---
+++ src/metadata/mod-loader-type.ts
@@ -0,0 +1,16 @@
+enum ModLoaderType {
+    Fabric = 1,
+    Forge,
+}
+
+namespace ModLoaderType {
+    export function getValues(): ModLoaderType[] {
+        return <ModLoaderType[]>Object.values(ModLoaderType).filter(x => typeof x === "number");
+    }
+
+    export function toString(target: ModLoaderType): string {
+        return ModLoaderType[target] ?? target.toString();
+    }
+}
+
+export default ModLoaderType;
