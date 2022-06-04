46c8fd7a6dc25df67a72bc677591f5cf1ba064d8
Fx Morin <fx.e.morin@gmail.com>
2022-06-04 13:47:38 -0400

    temp Modrinth Dependencies

--- src/publishing/modrinth/modrinth-publisher.ts
+++ src/publishing/modrinth/modrinth-publisher.ts
@@ -1,14 +1,29 @@
-import { createVersion } from "../../utils/modrinth-utils";
+import { createVersion, getProjectFromSlug } from "../../utils/modrinth-utils";
 import { File } from "../../utils/file";
 import ModPublisher from "../mod-publisher";
 import PublisherTarget from "../publisher-target";
+
+const modrinthDependencyKinds = new Map([
+    [DependencyKind.Depends, "required"],
+    [DependencyKind.Recommends, "optional"],
+    [DependencyKind.Suggests, "optional"],
+    [DependencyKind.Includes, ""],
+    [DependencyKind.Breaks, "incompatible"],
+]);
 
 export default class ModrinthPublisher extends ModPublisher {
     public get target(): PublisherTarget {
         return PublisherTarget.Modrinth;
     }
 
-    protected async publishMod(id: string, token: string, name: string, version: string, channel: string, loaders: string[], gameVersions: string[], _java: string[], changelog: string, files: File[]): Promise<void> {
+    protected async publishMod(id: string, token: string, name: string, version: string, channel: string, loaders: string[], gameVersions: string[], _java: string[], changelog: string, files: File[], dependencies: Dependency[]): Promise<void> {
+        const projects = dependencies
+            .filter((x, _, self) => (x.kind !== DependencyKind.Suggests && x.kind !== DependencyKind.Includes) || !self.find(y => y.id === x.id && y.kind !== DependencyKind.Suggests && y.kind !== DependencyKind.Includes))
+            .map(x => ({
+                project_id: (await getProjectFromSlug(x.getProjectSlug(this.target))).id, // Should probably allow id's here also
+                dependency_type: modrinthDependencyKinds.get(x.kind)
+            }))
+            .filter(x => x.project_id && x.dependency_type);
         const data = {
             version_title: name || version,
             version_number: version,
@@ -17,6 +32,7 @@ export default class ModrinthPublisher extends ModPublisher {
             game_versions: gameVersions,
             loaders,
             featured: true,
+            dependencies: !projects.length ? undefined : projects
         };
         await createVersion(id, data, files, token);
     }
--- src/utils/modrinth-utils.ts
+++ src/utils/modrinth-utils.ts
@@ -5,6 +5,51 @@ import { File } from "./file";
 import { computeHash } from "./hash-utils";
 import SoftError from "./soft-error";
 
+const baseUrl = "https://api.modrinth.com";
+
+interface ModrinthProject {
+    slug: string;
+    title: string;
+    description: string;
+    categories: string[];
+    client_side: string;
+    server_side: string;
+    body: string;
+    issues_url: string;
+    source_url: string;
+    wiki_url: string;
+    discord_url: string;
+    donation_urls: {
+        id: string;
+        platform: string;
+        url: string;
+    }[];
+    project_type: string;
+    downloads: number;
+    icon_url: string;
+    id: string;
+    team: string;
+    body_url: string?;
+    moderator_message: string?;
+    published: string;
+    updated: string;
+    followers: number;
+    status: string;
+    license: {
+        id: string;
+        name: string;
+        url: string;
+    };
+    versions: string[];
+    gallery: {
+        url: string;
+        featured: boolean;
+        title: string;
+        description: string;
+        created: string;
+    }[];
+}
+
 export async function createVersion(modId: string, data: Record<string, any>, files: File[], token: string): Promise<string> {
     data = {
         dependencies: [],
@@ -20,7 +65,7 @@ export async function createVersion(modId: string, data: Record<string, any>, fi
         form.append(i.toString(), await fileFromPath(file.path), file.name);
     }
 
-    const response = await fetch("https://api.modrinth.com/api/v1/version", {
+    const response = await fetch(`${baseUrl}/api/v1/version`, {
         method: "POST",
         headers: { Authorization: token },
         body: <any>form
@@ -41,6 +86,10 @@ export async function createVersion(modId: string, data: Record<string, any>, fi
         await makeFilePrimary(versionId, primaryFile.path, token);
     }
     return versionId;
+}
+
+export async function getProjectFromSlug(modSlug: string): Promise<ModrinthProject> {
+    return await (await fetch(`${baseUrl}/v2/project/slug/${modSlug}`)).json();
 }
 
 export async function makeFilePrimary(versionId: string, filePath: string, token: string): Promise<boolean> {
