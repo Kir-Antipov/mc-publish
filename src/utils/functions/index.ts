ce5227a2ce6bf5af2f02a30740e1337ccbc7b7ef
Kir_Antipov <kp.antipov@gmail.com>
2022-12-05 08:05:45 +0000

    Added `functions` module

---
+++ src/utils/functions/index.ts
@@ -0,0 +1,4 @@
+export { Func } from "./func";
+export { Callable, makeCallable } from "./callable";
+export { AsyncFunc } from "./async-func";
+export { Middleware, MiddlewareHandler } from "./middleware";
