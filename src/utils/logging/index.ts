13a303ce8d9c37a4dedeb687aef2485d2a80a8cf
Kir_Antipov <kp.antipov@gmail.com>
2022-12-20 04:18:29 +0000

    Added `logging` module

---
+++ src/utils/logging/index.ts
@@ -0,0 +1,13 @@
+export {
+    Logger,
+
+    NULL_LOGGER,
+    CONSOLE_LOGGER,
+    PROCESS_LOGGER,
+
+    getDefaultLogger,
+} from "./logger";
+
+export {
+    LoggingStopwatch,
+} from "./logging-stopwatch";
