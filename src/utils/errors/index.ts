919bc5d68e24a2db132193b64f0009d53394d4d8
Kir_Antipov <kp.antipov@gmail.com>
2023-01-12 10:19:32 +0000

    Added `errors` module

---
+++ src/utils/errors/index.ts
@@ -0,0 +1,6 @@
+export { isError } from "./error";
+export { ArgumentError } from "./argument-error";
+export { ArgumentNullError } from "./argument-null-error";
+export { SoftError, isSoftError } from "./soft-error";
+export { FileNotFoundError } from "./file-not-found-error";
+export { HttpError, isHttpError } from "./http-error";
