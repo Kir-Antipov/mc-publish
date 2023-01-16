8db475ab280d6cbcb3aad2059d2c0798f0c0ecbf
Kir_Antipov <kp.antipov@gmail.com>
2023-01-16 19:15:59 +0000

    Added `reflection` module

---
+++ src/utils/reflection/index.ts
@@ -0,0 +1,33 @@
+export {
+    ImportDirective,
+    ExecutedImportDirective,
+    ImportDirectiveExecutionOptions,
+
+    parseImportDirective,
+    formatImportDirective,
+
+    executeImportDirective,
+} from "./import-directive";
+
+export {
+    ModuleLoader,
+    NODE_MODULE_LOADER,
+    DYNAMIC_MODULE_LOADER,
+} from "./module-loader";
+
+export {
+    defineNestedProperty,
+    defineNestedProperties,
+
+    getPropertyDescriptor,
+    getAllPropertyDescriptors,
+
+    getAllKeys,
+    getAllNames,
+    getAllSymbols,
+    getAllValues,
+    getAllEntries,
+    getOwnEntries,
+
+    merge,
+} from "./object-reflector";
