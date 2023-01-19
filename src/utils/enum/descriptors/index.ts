0a98e5a66a256a8c4d662ba275cae85a2659b340
Kir_Antipov <kp.antipov@gmail.com>
2023-01-19 15:54:32 +0000

    Added `enum/descriptors` module

---
+++ src/utils/enum/descriptors/index.ts
@@ -0,0 +1,22 @@
+export {
+    EnumDescriptor,
+
+    getEnumDescriptorByUnderlyingType,
+    inferEnumDescriptorOrThrow,
+} from "./enum-descriptor";
+
+export {
+    BigIntDescriptor,
+} from "./bigint-descriptor";
+
+export {
+    BooleanDescriptor,
+} from "./boolean-descriptor";
+
+export {
+    NumberDescriptor,
+} from "./number-descriptor";
+
+export {
+    StringDescriptor,
+} from "./string-descriptor";
