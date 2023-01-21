5a2e5f176cd363cf07925fbe505e1377b46a9d18
Kir_Antipov <kp.antipov@gmail.com>
2023-01-21 15:21:34 +0000

    Added `enum` module

---
+++ src/utils/enum/index.ts
@@ -0,0 +1,21 @@
+export {
+    Enum,
+    EnumOptions,
+} from "./enum";
+
+export {
+    EnumKey,
+} from "./enum-key";
+
+export {
+    EnumValue,
+} from "./enum-value";
+
+export {
+    EnumEntry,
+} from "./enum-entry";
+
+export {
+    ENUM_SEPARATORS,
+    DEFAULT_ENUM_SEPARATOR,
+} from "./enum-separators";
