70b1eedf9106110e350a47cc35e875c9ac927af1
Kir_Antipov <kp.antipov@gmail.com>
2023-01-29 15:56:03 +0000

    Added `versioning` module

---
+++ src/utils/versioning/index.ts
@@ -0,0 +1,17 @@
+export {
+    VersionRange,
+
+    parseVersionRange,
+    anyVersionRange,
+    noneVersionRange,
+} from "./version-range";
+
+export {
+    VersionType,
+} from "./version-type";
+
+export {
+    Version,
+
+    parseVersion,
+} from "./version";
