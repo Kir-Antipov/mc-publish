fc540e9a7342fd602c4988abe617d0af201c9876
Kir_Antipov <kp.antipov@gmail.com>
2023-02-09 19:12:12 +0000

    Added `typescript` module

---
+++ src/utils/typescript/index.ts
@@ -0,0 +1,23 @@
+export { TypeScriptComment } from "./typescript-comment";
+export { TypeScriptDocument } from "./typescript-document";
+export { TypeScriptExport } from "./typescript-export";
+export { TypeScriptImport } from "./typescript-import";
+export { TypeScriptImports } from "./typescript-imports";
+export { TypeScriptInterface } from "./typescript-interface";
+export { TypeScriptTypeLiteral } from "./typescript-type-literal";
+export { TypeScriptTypeAlias } from "./typescript-type-alias";
+export { TypeScriptIntersectionType } from "./typescript-intersection-type";
+export { TypeScriptTypeDefinition } from "./typescript-type-definition";
+export { TypeScriptUnionType } from "./typescript-union-type";
+export { TypeScriptProperty } from "./typescript-property";
+export { TypeScriptObject } from "./typescript-object";
+export { CommentableTypeScriptNode } from "./commentable-typescript-node";
+export { TypeScriptVariable } from "./typescript-variable";
+export {
+    TypeScriptFormattingOptions,
+    getIndentation,
+    getNewline,
+    getQuotes,
+    getTab,
+    incrementIndent,
+} from "./typescript-formatting-options";
