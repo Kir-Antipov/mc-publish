4f60d7b56d5f0478a1e6ffb356d474dd3d55efd8
Kir_Antipov <kp.antipov@gmail.com>
2023-02-26 18:21:04 +0000

    Added `dependencies` module

---
+++ src/dependencies/index.ts
@@ -0,0 +1,14 @@
+export {
+    DependencyType,
+} from "./dependency-type";
+
+export {
+    Dependency,
+    DependencyInfo,
+    DependencyLike,
+
+    parseDependency,
+    createDependency,
+    formatDependency,
+    isDependency,
+} from "./dependency";
