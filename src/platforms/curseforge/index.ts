19fc58d31eeb5a5402a6dc2c7ca9ab778b6e1100
Kir_Antipov <kp.antipov@gmail.com>
2023-04-14 12:37:34 +0000

    Moved `curseforge` module

---
+++ src/platforms/curseforge/index.ts
@@ -0,0 +1,66 @@
+export {
+    CurseForgeUploadApiClient,
+    CurseForgeUploadApiOptions,
+
+    CURSEFORGE_UPLOAD_API_URL,
+} from "./curseforge-upload-api-client";
+
+export {
+    CurseForgeEternalApiClient,
+    CurseForgeEternalApiOptions,
+
+    CURSEFORGE_ETERNAL_API_URL,
+} from "./curseforge-eternal-api-client";
+
+export {
+    CurseForgeDependency,
+} from "./curseforge-dependency";
+
+export {
+    CurseForgeDependencyType,
+} from "./curseforge-dependency-type";
+
+export {
+    CurseForgeGameVersion,
+
+    CURSEFORGE_GAME_VERSION_PLUGIN_NAME_COMPARER,
+    CURSEFORGE_GAME_VERSION_SNAPSHOT_NAME_COMPARER,
+
+    findCurseForgeGameVersionIdsByNames,
+} from "./curseforge-game-version";
+
+export {
+    CurseForgeGameVersionMap,
+
+    createCurseForgeGameVersionMap,
+} from "./curseforge-game-version-map";
+
+export {
+    CurseForgeGameVersionType,
+} from "./curseforge-game-version-type";
+
+export {
+    CurseForgeGameVersionUnion,
+} from "./curseforge-game-version-union";
+
+export {
+    CurseForgeProject,
+
+    isCurseForgeProjectId,
+} from "./curseforge-project";
+
+export {
+    CurseForgeUploader,
+    CurseForgeUploaderOptions,
+    CurseForgeUploadRequest,
+    CurseForgeUploadReport,
+} from "./curseforge-uploader";
+
+export {
+    CurseForgeVersion,
+    CurseForgeVersionInit,
+} from "./curseforge-version";
+
+export {
+    CurseForgeFile,
+} from "./curseforge-file";
