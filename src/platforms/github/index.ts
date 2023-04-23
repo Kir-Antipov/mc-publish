758a17baaa540db04e71a2dbb17a5b199c155c67
Kir_Antipov <kp.antipov@gmail.com>
2023-04-23 17:18:01 +0000

    Added `platforms/github` module

---
+++ src/platforms/github/index.ts
@@ -0,0 +1,40 @@
+export {
+    GitHubApiClient,
+    GitHubApiOptions,
+
+    GITHUB_API_URL,
+    DEFAULT_GITHUB_API_URL,
+    GITHUB_API_VERSION,
+} from "./github-api-client";
+
+export {
+    GitHubContext,
+} from "./github-context";
+
+export {
+    GitHubRelease,
+    GitHubReleaseIdentifier,
+    GitHubReleaseInit,
+    GitHubReleasePatch,
+    GitHubReleaseAssetsPatch,
+} from "./github-release";
+
+export {
+    GitHubUploader,
+    GitHubUploaderOptions,
+    GitHubUploadRequest,
+    GitHubUploadReport,
+} from "./github-uploader";
+
+export {
+    GitHubUser,
+} from "./github-user";
+
+export {
+    GitHubReleaseAsset,
+    GitHubReleaseAssetIdentifier,
+} from "./github-release-asset";
+
+export {
+    GitHubWebhookPayload,
+} from "./github-webhook-payload";
