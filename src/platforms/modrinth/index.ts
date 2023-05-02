0dd215e0d7237d722b66e1915d8d600d7cf7100f
Kir_Antipov <kp.antipov@gmail.com>
2023-05-02 09:34:10 +0000

    Moved `modrinth` module

---
+++ src/platforms/modrinth/index.ts
@@ -0,0 +1,47 @@
+export {
+    ModrinthApiClient,
+    ModrinthApiOptions,
+
+    MODRINTH_API_URL,
+    MODRINTH_STAGING_API_URL,
+} from "./modrinth-api-client";
+
+export {
+    ModrinthDependencyType,
+} from "./modrinth-dependency-type";
+
+export {
+    ModrinthDependency,
+} from "./modrinth-dependency";
+
+export {
+    ModrinthGameVersion,
+} from "./modrinth-game-version";
+
+export {
+    ModrinthLoader,
+} from "./modrinth-loader";
+
+export {
+    ModrinthProject,
+    ModrinthProjectPatch,
+} from "./modrinth-project";
+
+export {
+    ModrinthUnfeatureMode,
+} from "./modrinth-unfeature-mode";
+
+export {
+    ModrinthVersion,
+    ModrinthVersionInit,
+    ModrinthVersionPatch,
+    ModrinthVersionSearchTemplate,
+    UnfeaturableModrinthVersion,
+} from "./modrinth-version";
+
+export {
+    ModrinthUploader,
+    ModrinthUploaderOptions,
+    ModrinthUploadRequest,
+    ModrinthUploadReport,
+} from "./modrinth-uploader";
