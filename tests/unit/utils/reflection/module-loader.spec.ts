98e9aced204fc4f5aa942480d3f73eb865dd5027
Kir_Antipov <kp.antipov@gmail.com>
2023-05-20 15:10:21 +0300

    Removed unnecessary `async`

--- tests/unit/utils/reflection/module-loader.spec.ts
+++ tests/unit/utils/reflection/module-loader.spec.ts
@@ -1,7 +1,7 @@
 import { NODE_MODULE_LOADER, DYNAMIC_MODULE_LOADER } from "@/utils/reflection/module-loader";
 
 describe("NODE_MODULE_LOADER", () => {
-    test("returns undefined if a module cannot be loaded", async () => {
+    test("returns undefined if a module cannot be loaded", () => {
         // https://github.com/nodejs/node/issues/35889
         // Jest throws segfault if we try to actually test it, so...
         expect(NODE_MODULE_LOADER).toBeDefined();
