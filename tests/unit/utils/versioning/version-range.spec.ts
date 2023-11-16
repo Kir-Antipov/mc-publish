868348bcadae8e82b1c7b7cf3046eaf030b792dd
Luca v. d. W <github@luken.cc>
2023-11-16 20:18:32 +0100

    Added test cases for forge style single version "range"

--- tests/unit/utils/versioning/version-range.spec.ts
+++ tests/unit/utils/versioning/version-range.spec.ts
@@ -35,6 +35,34 @@ describe("parseVersionRange", () => {
         expect(versionRange.includes("2.0.1")).toBe(false);
     });
 
+    test("parses an interval notation string with single version correctly", () => {
+        const versionRange = parseVersionRange("[1.2.3]");
+
+        expect(versionRange).toBeDefined();
+        expect(versionRange.toString()).toBe("[1.2.3]");
+
+        expect(versionRange.includes("1.2.3")).toBe(true);
+        expect(versionRange.includes("1.2.4")).toBe(false);
+        expect(versionRange.includes("2.0.0")).toBe(false);
+        expect(versionRange.includes("1.0.0")).toBe(false);
+        expect(versionRange.includes("1.2.2")).toBe(false);
+        expect(versionRange.includes("2.0.1")).toBe(false);
+    });
+
+    test("parses an interval notation string with single version open end correctly", () => {
+        const versionRange = parseVersionRange("[1.2.3,)");
+
+        expect(versionRange).toBeDefined();
+        expect(versionRange.toString()).toBe("[1.2.3,)");
+
+        expect(versionRange.includes("1.2.3")).toBe(true);
+        expect(versionRange.includes("1.2.4")).toBe(true);
+        expect(versionRange.includes("2.0.0")).toBe(true);
+        expect(versionRange.includes("2.0.1")).toBe(true);
+        expect(versionRange.includes("1.0.0")).toBe(false);
+        expect(versionRange.includes("1.2.2")).toBe(false);
+    });
+
     test("parses a string that mixes semver and interval notation correctly", () => {
         const versionRange = parseVersionRange(">=1.2.3 <2.0.0 || [2.0.0,3.0.0)");
 
