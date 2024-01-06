b77098a9f6f641b42640e3f6c97b1a64388c34e6
Kir_Antipov <kp.antipov@gmail.com>
2024-01-06 11:46:05 +0000

    Added tests for `minecraft`

---
+++ tests/unit/games/minecraft/minecraft.spec.ts
@@ -0,0 +1,7 @@
+import { MINECRAFT } from "@/games/minecraft/minecraft";
+
+describe("MINECRAFT", () => {
+    test("is string", () => {
+        expect(typeof MINECRAFT).toBe("string");
+    });
+});
